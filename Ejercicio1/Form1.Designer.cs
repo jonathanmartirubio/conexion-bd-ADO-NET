24254424663c91634a0ba40b914ec7bfee9c4fea
jonathanmartirubio <jonamr09@gmail.com>
2022-05-11 12:02:50 +0200

    Correción de la conexión a la BD

--- Ejercicio1/Form1.Designer.cs
+++ Ejercicio1/Form1.Designer.cs
@@ -29,21 +29,133 @@ namespace Ejercicio1
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellidos = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbDNI = new System.Windows.Forms.TextBox();
+            this.tbTelf = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DNI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tlf:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nombre:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(149, 97);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(225, 22);
+            this.tbNombre.TabIndex = 5;
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.Location = new System.Drawing.Point(508, 73);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(225, 22);
+            this.tbApellidos.TabIndex = 6;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(508, 125);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(225, 22);
+            this.tbEmail.TabIndex = 7;
+            // 
+            // tbDNI
+            // 
+            this.tbDNI.Location = new System.Drawing.Point(149, 49);
+            this.tbDNI.Name = "tbDNI";
+            this.tbDNI.Size = new System.Drawing.Size(225, 22);
+            this.tbDNI.TabIndex = 8;
+            // 
+            // tbTelf
+            // 
+            this.tbTelf.Location = new System.Drawing.Point(149, 145);
+            this.tbTelf.Name = "tbTelf";
+            this.tbTelf.Size = new System.Drawing.Size(225, 22);
+            this.tbTelf.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbTelf);
+            this.Controls.Add(this.tbDNI);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Ejercicio 1";
+            this.Text = "Ejercicio 1 - Profesores";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellidos;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbDNI;
+        private System.Windows.Forms.TextBox tbTelf;
     }
 }
 
--- Ejercicio1/Form1.cs
+++ Ejercicio1/Form1.cs
@@ -20,10 +20,24 @@ namespace Ejercicio1
 
         DataSet dsProfesores;
         SqlDataAdapter dataAdapter;
+        private int pos;
+
+        private void MostrarRegistro(int pos)
+        {
+            DataRow drRegistro;
+
+            drRegistro = dsProfesores.Tables["Profesores"].Rows[pos];
+
+            tbDNI.Text = drRegistro["DNI"].ToString();
+            tbNombre.Text = drRegistro["Nombre"].ToString();
+            tbTelf.Text = drRegistro["Tlf"].ToString();
+            tbApellidos.Text = drRegistro["Apellido"].ToString();
+            tbEmail.Text = drRegistro["email"].ToString();
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string cadConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Instituto.mdf;Integrated Security=True;Connect Timeout=30";
+            string cadConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\FA506IV\\source\\repos\\jonathanmartirubio\\conexion-bd-ADO-NET\\Instituto.mdf;Integrated Security=True;Connect Timeout=30";
 
             SqlConnection conect = new SqlConnection(cadConexion);
 
@@ -35,6 +49,9 @@ namespace Ejercicio1
             dsProfesores = new DataSet();
 
             dataAdapter.Fill(dsProfesores, "Profesores");
+
+            pos = 0;
+            MostrarRegistro(pos);
 
             conect.Close();
         }

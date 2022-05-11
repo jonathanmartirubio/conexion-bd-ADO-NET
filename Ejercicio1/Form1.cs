7654f7ccb8f06c279b50d4b33e94a366790e1bc4
jonathanmartirubio <jonamr09@gmail.com>
2022-05-11 11:24:47 +0200

    Conexión con la base de datos

--- Ejercicio1/Form1.cs
+++ Ejercicio1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,13 @@ namespace Ejercicio1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string cadConexion = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Instituto.mdf;Integrated Security=True;Connect Timeout=30";
 
+            SqlConnection conect = new SqlConnection(cadConexion);
+
+            conect.Open();
+
+            conect.Close();
         }
     }
 }
